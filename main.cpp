513d72aee4031db6096a9133d8e037081e168092
Fedorinov Evgeny <dambasss007@mail.ru>
2016-09-23 12:00:23 +0600

    git log

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
+
+int main(int argc, char **argv)
+
+{
+int k=0;
+printf("Vvod pervogo 4isla:\n");
+		std::cin>>k;
+int p=0;
+printf("Vvod vtorogo 4isla:\n");
+		std::cin>>p;
+int s=0;
+s=p+k;
+printf("symma 4isel:\n");
+std::cout<<s<<std::endl;
+return 0;
+}
+
